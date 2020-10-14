7933757c33e964e62e405449ddb736437eae06f8
superbobr <leonseoprofi@gmail.com>
2020-10-14 20:39:35 +0300

    The solution of the proplem ReverseInt

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
-}
+  let revNumber = '' + n;
+  if (revNumber[0] === '-') {
+      revNumber = revNumber.slice(1);
+  }
+  return +(revNumber.split('').reverse().join(''));
+};
